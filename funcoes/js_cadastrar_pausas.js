33cd7c9549064d87780e9b13d59c5e14c1add270
LucasAguiar00 <115197020+LucasAguiar00@users.noreply.github.com>
2023-06-06 21:52:10 -0300

    Delete funcoes directory

--- funcoes/js_cadastrar_pausas.js
+++
@@ -1,8 +0,0 @@
-function atualizar_nome(){
-    var usuario
-    var saudacao = window.document.getElementById("saudacao");
-
-    usuario =  localStorage.getItem('valueText');
-
-    saudacao.innerHTML = `Cadastro de pausas: ${usuario}`
-}
